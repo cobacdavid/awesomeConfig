a6b137eba18748e217893c41d93ed769b1a5e57a
cobacdavid <david.cobac@gmx.fr>
2020-03-14 14:14:42 +0100

    Delete myPRIVATEvariables.lua

--- myPRIVATEvariables.lua
+++
@@ -1,6 +0,0 @@
-lastfmUser      = "AmI2Blame4"
-garminUser      = "AmI2Blame4"
-ftpNASUser      = "david"
-githubUser      = "cobacdavid"
-flickrUser      = "127316223@N07"
-flickrUserAlt   = "127316223%40N07"
